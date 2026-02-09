7ba87a4c21d07f76e31316f1859e2be9451128e1
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-09 13:23:00 -0300

    Create Program.Usuario.cs

---
+++ Sistema-de-Gerenciamento-de-Usuarios/Program.Usuario.cs
@@ -0,0 +1,14 @@
+﻿namespace SistemaGenrenciamentoDeUsuarios
+{
+    internal partial class Program
+    {
+        //Criar uma classe chamada Usuario.
+        //criar:ID, Nome, Email
+        public class Usuario
+        {
+            public int Id { get; set; }
+            public string Nome { get; set; }
+            public string Email { get; set; }
+        }
+    }
+}
